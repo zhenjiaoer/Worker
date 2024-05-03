ec268948edf3e36ab68bbac8d0f5aa169131cf63
zhenjiaoer <159562857+zhenjiaoer@users.noreply.github.com>
2024-05-03 20:04:37 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -29,7 +29,7 @@ let addressesnotlsapi = [
 
 let DLS = 8;//速度下限
 let addressescsv = [
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv', //iptest测速结果文件。
+	//'https://raw.githubusercontent.com/zhenjiaoer/Worker/main/a.csv', //iptest测速结果文件。
 ];
 
 let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
